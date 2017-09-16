36239fce643d56ba695c85966f95a02de8f5167f
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-09-16 18:39:18 +0100

    Remove all json files to replace with firebase
    Write module to work with firebase

---
+++ get-data.js
@@ -0,0 +1,71 @@
+const ROOT_KEY = 'cms_data_config',
+      FIREBASE_KEY = 'firebase_url',
+      DIRECTORY_KEY = 'data_directory',
+      DATA_FILENAME = 'data.json';
+
+const fs = require('fs-extra'),
+      curl = require('curl'),
+      config = require('./package.json')[ROOT_KEY],
+      path = require('path');
+      
+
+if (config === null  || config === undefined
+                     || !config.hasOwnProperty(FIREBASE_KEY)
+                     || !config.hasOwnProperty(DIRECTORY_KEY)) {
+   throw `Missing information, please ensure your package json contains the following attributes
+   "${ROOT_KEY}": {
+      "${FIREBASE_KEY}": "<your firebase url>",
+      "${DIRECTORY_KEY}": "<directory to save data e.g ./dist>"
+   }`;
+
+   return;
+}
+
+const dataPath = path.format({
+         dir: config[DIRECTORY_KEY],
+         base: DATA_FILENAME
+      }).toString();
+
+module.exports.getData = (discardCache = false, createCache = true) => {   
+   if (discardCache || !cacheExists()) {
+      return fetchFromFirebase(createCache);
+   } else {
+      return loadLocalCache();
+   }
+}
+
+function fetchFromFirebase() {
+   return "ginjerogijrgoij";
+}
+
+function cacheExists() {  
+   return fs.existsSync(dataPath);
+}
+
+function fetchFromFirebase(createCache) {
+   return new Promise((resolve, reject) => {
+      curl.get(config[FIREBASE_KEY] + '.json', {}, (err, response, body) => {
+         if (err) reject(err);
+         if (createCache) writeToCache(body);
+         resolve(body);
+      });
+   });
+}
+
+function loadLocalCache() {
+   return new Promise((resolve, reject) => {
+      fs.readFile(dataPath, 'utf-8', (err, data) => {
+         if (err) reject(err);
+         console.log(dataPath);
+
+         resolve(data);
+      });
+   });
+}
+
+function writeToCache(body) {
+   const json = JSON.parse(body);
+   fs.writeFile(dataPath, JSON.stringify(json, null, 3), (err) => {
+      if (err) throw err;
+   });
+}
