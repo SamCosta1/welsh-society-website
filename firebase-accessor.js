94e03e7838be35b9dc253c0087d87a30fc360f2d
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-09-19 21:27:28 +0100

    Intellij reformat and update data file

--- firebase-accessor.js
+++ firebase-accessor.js
@@ -1,24 +1,24 @@
 const ROOT_KEY = 'cms_data_config';
 const fs = require('fs-extra'),
-      firebase = require('firebase'),
-      config = require('./package.json')[ROOT_KEY],
-      url = require('url'),
-      path = require('path');
+   firebase = require('firebase'),
+   config = require('./package.json')[ROOT_KEY],
+   url = require('url'),
+   path = require('path');
 
 const DATA_ENDPOINT_KEY = 'firebase_data_endpoint',
-      DIRECTORY_KEY = 'data_directory',
-      DATA_FILENAME = 'data.json',
-      FIREBASE_ACCESS_TOKEN_KEY = 'firebase_access_key_file',
-      FIREBASE_CONFIG_KEY = 'firebase_config_file',
-      FIREBASE_SITEDATA_ENDPOINT = config[DATA_ENDPOINT_KEY];
+   DIRECTORY_KEY = 'data_directory',
+   DATA_FILENAME = 'data.json',
+   FIREBASE_ACCESS_TOKEN_KEY = 'firebase_access_key_file',
+   FIREBASE_CONFIG_KEY = 'firebase_config_file',
+   FIREBASE_SITEDATA_ENDPOINT = config[DATA_ENDPOINT_KEY];
 
-if (config === null  || config === undefined
-                     || !config.hasOwnProperty(FIREBASE_ACCESS_TOKEN_KEY)
-                     || !config.hasOwnProperty(FIREBASE_CONFIG_KEY)
-                     || !config.hasOwnProperty(DIRECTORY_KEY)) {
+if (config === null || config === undefined
+   || !config.hasOwnProperty(FIREBASE_ACCESS_TOKEN_KEY)
+   || !config.hasOwnProperty(FIREBASE_CONFIG_KEY)
+   || !config.hasOwnProperty(DIRECTORY_KEY)) {
    throw `Missing information, please ensure your package json contains the following attributes
    "${ROOT_KEY}": {
-      "${FIREBASE_CONFIG_KEY}": "<directory to save data e.g ./dist>",
+         "${FIREBASE_CONFIG_KEY}": "<directory to save data e.g ./dist>",
       "${FIREBASE_ACCESS_TOKEN_KEY}": "<directory to save data e.g ./dist>",
       "${DIRECTORY_KEY}": "<directory to save data e.g ./dist>"
    }`;
@@ -27,28 +27,39 @@ if (config === null  || config === undefined
 }
 
 const dataPath = path.format({
-         dir: config[DIRECTORY_KEY],
-         base: DATA_FILENAME
-      }).toString();
+   dir: config[DIRECTORY_KEY],
+   base: DATA_FILENAME
+}).toString();
 
-module.exports.getData = (discardCache = false, createCache = true) => {   
+module.exports.getDataPath = () => {
+   return dataPath;
+};
+
+
+module.exports.getData = (discardCache = false, createCache = true) => {
    if (discardCache || !cacheExists()) {
       return fetchFromFirebase(createCache);
    } else {
       return loadLocalCache();
    }
-}
+};
 
 module.exports.pushLocalToFirebase = () => {
    initFirebase();
    authenticateFirebase().then(() => {
       loadLocalCache().then(data => {
-      firebase.database().ref(FIREBASE_SITEDATA_ENDPOINT).set(data)
-            .then(() => { process.exit() })
-            .catch(err => { throw err; });
-      }).catch(err => { throw err });
+         firebase.database().ref(FIREBASE_SITEDATA_ENDPOINT).set(data)
+            .then(() => {
+               process.exit()
+            })
+            .catch(err => {
+               throw err;
+            });
+      }).catch(err => {
+         throw err
+      });
    });
-}
+};
 
 function initFirebase() {
    const firebaseConfig = require(config[FIREBASE_CONFIG_KEY]).config;
@@ -58,26 +69,20 @@ function initFirebase() {
 function authenticateFirebase() {
    const firebaseAuthConfig = require(config[FIREBASE_ACCESS_TOKEN_KEY])
    return firebase.auth().signInWithEmailAndPassword(firebaseAuthConfig.email, firebaseAuthConfig.password)
-               .catch(err => { throw err });
+      .catch(err => { throw err  });
 }
 
-function cacheExists() {  
+function cacheExists() {
    return fs.existsSync(dataPath);
 }
 
 function fetchFromFirebase(createCache) {
    initFirebase();
-   return new Promise((resolve, reject) => {
+   return firebase.database().ref(FIREBASE_SITEDATA_ENDPOINT).once('value').then(snapshot => {
 
-      firebase.database().ref(FIREBASE_SITEDATA_ENDPOINT).once('value').then(snapshot => {         
-         if (createCache) writeToCache(snapshot.val(), done);
-         else done();
-      
-         function done() {
-            resolve(snapshot.val());
-         }
-
-      }).catch(err => { throw err });
+      firebase.database().goOffline();
+      writeToCache(snapshot.val());
+      return snapshot.val();
    });
 }
 
@@ -91,9 +96,6 @@ function loadLocalCache() {
    });
 }
 
-function writeToCache(body, cb) {
-   fs.writeFile(dataPath, JSON.stringify(body, null, 3), (err) => {
-      if (err) throw err;
-      cb();
-   });
+function writeToCache(body) {
+   fs.writeFileSync(dataPath, JSON.stringify(body, null, 3));
 }
